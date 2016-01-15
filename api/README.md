e5d9d633842f0af1948346443134f49ddd91ad19
Ben Janecke <benjanecke@gmail.com>
2016-01-15 10:43:10 +0200

    Move api application into api folder
    Okay so I could have been better about creating atomic commits here but here goes 
    
    * Added .env files for great good 
    * Added a babel.rc file for ES2015 loveliness
    * Refactored the code to use ES2015
    * Cried a little at how beautiful this is all going to be

---
+++ api/README.md
@@ -0,0 +1,16 @@
+# Sushi Scaffold
+
+A scaffold for a ES2015, Express node api.
+
+# Requirements
+
+* node ^4.0.0
+* babel-cli
+
+# Getting started
+
+```
+npm install -g babel-cli
+cp .env.example .env
+env $(cat .env) npm start // starts a server
+```
---
+++ api/app.js
@@ -0,0 +1,59 @@
+import express from 'express';
+import path from 'path';
+import favicon from 'serve-favicon';
+import logger from 'morgan';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+import routes from './routes/index';
+import engine from 'ejs-locals';
+
+const app = express();
+
+// This has to be here :| LAAAAME!
+app.set('views', path.join(__dirname, 'views'));
+app.set('engine', engine);
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+if (app.get('env') === 'development') {
+  // will print stacktrace
+  // development error handler
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+} else {
+  // production error handler
+  // no stacktraces leaked to user
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: {}
+    });
+  });
+}
+
+
+
+export default app;
