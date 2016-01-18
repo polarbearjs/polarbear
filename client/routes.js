30bd3434e399ef40532d5e226a3d2783a6ec0da7
Ben Janecke <benjanecke@gmail.com>
2016-01-18 15:29:17 +0200

    Use babel-register for production code
    And since we believe in dev prod parity we use it for development as well

--- client/routes.js
+++ client/routes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { IndexRoute, Route } from 'react-router';
-import { App, Home } from '../containers';
+import { App, Home } from './containers';
 
 export default () => {
   return (
