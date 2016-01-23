97dfc60e54c2d353504f3d64183e3c1cb68af0cf
Ben Janecke <benjanecke@gmail.com>
2016-01-23 17:08:58 +0200

    Was moving in completely the wrong direction here
    Polarbear wants to be simple and understandable, it’s going to take a lot of time to get there but the universal client app needs to be simple to understand and what I had here was not.

--- client/redux/helpers/get-data-dependencies.js
+++
@@ -1,8 +0,0 @@
-export default (components, getState, dispatch, location, params, deferred) => {
-  const methodName = deferred ? 'fetchDataDeferred' : 'fetchData';
-
-  return components
-    .filter((component) => component && component[methodName])
-    .map((component) => component[methodName])
-    .map(fetchData => fetchData(getState, dispatch, location, params));
-};
